9d1ad230a0db7dc6a1b169878c615dc4ec9ee539
Jan-Philip Loos <maxdaten@gmail.com>
2013-06-24 13:32:01 +0200

    small code cleaning

--- src/Ecom/Handler/Home.hs
+++ src/Ecom/Handler/Home.hs
@@ -11,16 +11,16 @@ getHomeR = do
     allProducts <- acidQuery (AllProducts)
     prodImg <- widgetToPageContent $ placeholditWidget 250 210
 
-    mUsername <- lookupSession "name" :: Handler (Maybe Text)
-    mUser <- fuser mUsername :: Handler (Maybe User)
-    let userHistory = join . maybeToList $ history <$> mUser :: ([Product])
+    mUser <- fuser =<< lookupSession "name"
+    let userHistory = join . maybeToList $ history <$> mUser
     recommendedProducts <- acidQuery (SimilarProducts userHistory 5.0)
 
     defaultLayout $ do
         aDomId <- newIdent
         setTitle "Willkommen!"
         $(widgetFile "homepage")
-        unless (null recommendedProducts) $(widgetFile "recommended-products")
+        unless (null recommendedProducts) 
+            $(widgetFile "recommended-products")
         $(widgetFile "product-catalog")
     where
         fuser :: Maybe Text -> Handler (Maybe User) 
