d63762dab6d2d040d9b6a3429d82d84bcd0b9971
Jan-Philip Loos <maxdaten@gmail.com>
2013-07-04 02:09:19 +0200

    StateManager exports now users and assocs too

--- tools/StateManager.hs
+++ tools/StateManager.hs
@@ -2,77 +2,94 @@
 
 module StateManager where
 
-import				Prelude	hiding (all)
-import				Control.Monad
-import 				Control.Exception 						(bracket)
-import 				System.Directory
-import 				System.FilePath
-import 				System.Console.CmdArgs.Implicit
-import				Data.List								(isSuffixOf, isPrefixOf)
-import				Data.Maybe 								(catMaybes)
+import              Prelude hiding                  (all)
+import              Control.Monad
+import              Control.Exception               (bracket)
+import              System.Directory
+import              System.FilePath
+import              System.Console.CmdArgs.Implicit
+import              Data.List                       (isSuffixOf, isPrefixOf)
+import              Data.Maybe                      (catMaybes)
+import qualified    Data.Text                       as T
 ----------------------------------------------------------------------------------------------------
-import 				Data.Acid
-import 				Data.Acid.Advanced
-import qualified    Data.Aeson                 as Aeson
-import           	Data.Aeson                 (FromJSON)
-import qualified	Data.ByteString.Lazy	   as BS
-import qualified    Data.UUID                  as UUID
+import              Data.Acid
+import              Data.Acid.Advanced
+import qualified    Data.Aeson                      as Aeson
+import              Data.Aeson                      (FromJSON, ToJSON)
+import qualified    Data.ByteString.Lazy            as BS
+import qualified    Data.UUID                       as UUID
 ----------------------------------------------------------------------------------------------------
-import 				Ecom.Model
+import              Ecom.Model
 ----------------------------------------------------------------------------------------------------
 
-data SMModes 		= Import { inDir :: String
-							 , purge :: Bool
-							 }
-                    | Export { outDir :: String
-                             }
-			 		| Query  { all               :: Bool
-                             , queryProducts     :: Bool
-                             , queryAssosiations :: Bool
-                             }
-			 		| Purge
-	deriving (Show, Data, Typeable)
+data SMModes    = Import { inDir          :: String
+                         , purge          :: Bool
+                         , importUsers    :: Bool
+                         , importProducts :: Bool
+                         , importAssocs   :: Bool
+                         }
+                | Export { outDir            :: String
+                         , exportUsers       :: Bool
+                         , exportProducts    :: Bool
+                         , exportAssocs      :: Bool
+                         }
+                | Query  { all               :: Bool
+                         , queryProducts     :: Bool
+                         , queryAssosiations :: Bool
+                         , queryUsers        :: Bool
+                         }
+                | Purge
+  deriving (Show, Data, Typeable)
 
 ----------------------------------------------------------------------------------------------------
 -- | directory definitions
 defDir      = "samples"
-productDir  = \i -> combine i "products"
-asscoDir    = \i -> combine i "assocs"
+defExports  = "exports"
+productDir  = "products"
+asscoDir    = "assocs"
+userDir     = "users"
 
 -- | programm mode definitions
 queryDef  = Query  { all = True                &= help "query everything in current state"
                    , queryProducts      = def  &= name "products"         &= help "show all products in state"
-                   , queryAssosiations  = def  &= name "assosiations"     &= help "show all assosiations" }
+                   , queryAssosiations  = def  &= name "assosiations"     &= help "show all assosiations" 
+                   , queryUsers         = def  &= name "users"            &= help "show all users" }
 importDef = Import { inDir = defDir
-                  &= opt defDir
-                  &= typDir
-                  &= help ("input directory for samples [default: \"" ++ defDir ++ "\"]")
+                          &= opt defDir
+                          &= typDir
+                          &= help ("input directory for samples [default: \"" ++ defDir ++ "\"]")
                    , purge = def &= help "delete current state before import"
+                   , importUsers    = def &= name "users"    &= help "import users"
+                   , importProducts = def &= name "products" &= help "import products"
+                   , importAssocs   = def &= name "assocs"   &= help "import assocs"
                    }
 purgeDef  = Purge
-exportDef = Export  { outDir = defDir
-                             &= opt defDir
-                             &= typDir
-                             &= help ("output directory for exported state [default: \"" ++ defDir ++ "\"]")
+exportDef = Export  { outDir = defExports
+                            &= opt defExports
+                            &= typDir
+                            &= help ("output directory for exported state [default: \"" ++ defExports ++ "\"]")
+                    , exportUsers    = def &= name "users"      &= help "export users"
+                    , exportProducts = def &= name "products"   &= help "export producs"
+                    , exportAssocs   = def &= name "assocs"     &= help "export assocs"
                     }
 
-argDef = modes $ [ queryDef  	&= help "query state" &= auto
-                 , importDef    &= help "import json samples in to state"
-				 , exportDef 	&= help "export state to json"
-				 , purgeDef		&= help "empty current state"
-				 ] &= summary "state-manager v0.1"
-				   &= program "state-manager"
+argDef = modes $ [ queryDef   &= help "query state" &= auto
+                 , importDef  &= help "import json samples in to state"
+                 , exportDef  &= help "export state to json"
+                 , purgeDef   &= help "empty current state"
+                 ] &= summary "state-manager v0.2"
+                   &= program "state-manager"
 
 ----------------------------------------------------------------------------------------------------
 
 main :: IO ()
 main = do
-	args <- cmdArgs argDef
-	print args
-	bracket
-		(openLocalState initialEcomState)
-		(closeAcidState)
-		(runStateManager args)
+  args <- cmdArgs argDef
+  print args
+  bracket
+    (openLocalState initialEcomState)
+    (closeAcidState)
+    (runStateManager args)
 
 ----------------------------------------------------------------------------------------------------
 -- | Import
@@ -80,9 +97,17 @@ runStateManager :: SMModes -> AcidState EcomState -> IO ()
 runStateManager Import{..} state = do
     when purge $ update state (PutState initialEcomState)
 
+    let all = (importProducts || importAssocs || importUsers) `xor` True
+
     -- import with [State-Update] from [Directory] - [Maybe Query] to check state
-    importWith InsertProduct (productDir inDir) $ Just AllProducts
-    importWith CombineAssoc (asscoDir inDir) $ Just AllAssocs
+    when (all || importProducts) $
+        importWith InsertProduct (inDir </> productDir) $ Just AllProducts
+    
+    when (all || importAssocs) $
+        importWith CombineAssoc (inDir </> asscoDir) $ Just AllAssocs
+
+    when (all || importUsers) $ 
+        importWith InsertUser (inDir </> userDir) $ Just AllUsers
 
     where
         filterSamples :: [FilePath] -> [FilePath]
@@ -120,9 +145,9 @@ runStateManager Import{..} state = do
                 _ -> return ()
 
 ----------------------------------------------------------------------------------------------------
--- | Import
+-- | Query
 runStateManager arg@Query{..} state = do
-    let all' = all `xor` (queryProducts || queryAssosiations)
+    let all' = all `xor` (queryProducts || queryAssosiations || queryUsers)
 
     when (all' || queryProducts) $ do
         stateProducts <- query state AllProducts
@@ -134,30 +159,58 @@ runStateManager arg@Query{..} state = do
         print "all assosiations:"
         mapM_ print stateAssocs
 
-    where
-        xor :: Bool -> Bool -> Bool
-        xor True p = not p
-        xor False p = p
+    when (all' || queryUsers) $ do
+        stateUsers <- query state AllUsers
+        print "all users:"
+        mapM_ print stateUsers
 
 ----------------------------------------------------------------------------------------------------
 -- | Purge
 runStateManager arg@Purge{..} state = update state (PutState initialEcomState)
+
 ----------------------------------------------------------------------------------------------------
 -- | Export
 runStateManager arg@Export{..} state = do
-    stateProducts <- query state AllProducts
+    let all = (exportProducts || exportAssocs || exportUsers) `xor` True
 
-    print "export following entries:"
-    mapM_ print stateProducts
-
-    createDirectoryIfMissing True outDir
-
-    let jsonProducts = zip stateProducts $ map Aeson.encode stateProducts
-    mapM_ (writeEncoded outDir) jsonProducts
+    exportWith AllProducts (outDir </> productDir)
+    exportWith AllAssocs (outDir </> asscoDir)
+    exportWith AllUsers (outDir </> userDir)
 
     where
-        writeEncoded :: FilePath -> (Product, BS.ByteString) -> IO ()
-        writeEncoded outDir (prod, jsonString) = BS.writeFile (combine outDir (getProductFilename prod)) jsonString
-        getProductFilename prod = (UUID.toString . unProductId . productId $ prod) <.> "json"
+        exportWith :: (QueryEvent queryEvent, MethodState queryEvent ~ EcomState, MethodResult queryEvent ~ [a]
+                      , ToJSON a, Show a, Fileable a) 
+                   => queryEvent -> FilePath -> IO ()
+        exportWith stQuery outDir = do
+            states <- query state stQuery
+
+            print "export following entries:"
+            mapM_ print states
+
+            createDirectoryIfMissing True outDir
+
+            let jsons = zip states $ map Aeson.encode states
+            mapM_ (writeEncoded outDir) jsons            
+
+        writeEncoded :: (ToJSON a, Fileable a) => FilePath -> (a, BS.ByteString) -> IO ()
+        writeEncoded outDir (st, jsonString) = BS.writeFile (outDir </> getFilename st <.> "json") jsonString
+
+xor :: Bool -> Bool -> Bool
+xor True p  = not p
+xor False p = p
 
 
+class Fileable a where
+    getFilename :: a -> FilePath
+
+
+instance Fileable Association where
+    getFilename = T.unpack . unProductCategory . assocCategory
+
+
+instance Fileable Product where
+    getFilename = UUID.toString . unProductId . productId
+
+
+instance Fileable User where
+    getFilename = T.unpack . username
